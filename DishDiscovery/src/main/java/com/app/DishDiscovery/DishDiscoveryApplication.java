72844253983d0ae3e6e4d3e4ed7622df256b2401
Калин Тошев <kalin.toshev84@gmail.com>
2023-08-11 14:08:43 +0300

    Add project to GitHub.

---
+++ DishDiscovery/src/main/java/com/app/DishDiscovery/DishDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.app.DishDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishDiscoveryApplication.class, args);
+	}
+
+}
---
+++ DishDiscovery/src/test/java/com/app/DishDiscovery/DishDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.DishDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
